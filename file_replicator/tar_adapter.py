064dab5e50535920f345f03d0c0cbf4d6866e5c9
Petre Mierlutiu <petrem@users.noreply.github.com>
2019-01-27 19:01:18 +0200

    add a tar adapter for gnu, bsd and busybox tar

---
+++ file_replicator/tar_adapter.py
@@ -0,0 +1,150 @@
+from abc import ABCMeta, abstractmethod
+import subprocess
+
+
+__all__ = [
+    "GnuTarAdapter",
+    "BsdTarAdapter",
+    "BusyBoxTarAdapter",
+    "detect_local_tar",
+    "detect_remote_tar",
+]
+
+
+class AbstractTarAdapter(metaclass=ABCMeta):
+    @abstractmethod
+    def __str__(self):
+        raise NotImplementedError
+
+    def __repr__(self):
+        cls = type(self)
+        return (
+            f"<{cls.__module__}.{cls.__name__} ({str(self)}) object at 0x{id(self):x}>"
+        )
+
+    @property
+    def cmd(self):
+        return "tar"
+
+    @abstractmethod
+    def receiver_options(self):
+        raise NotImplementedError
+
+    @abstractmethod
+    def sender_options(self, src_file):
+        raise NotImplementedError
+
+    def receiver_cmd(self):
+        return [self.cmd] + self.receiver_options()
+
+    def receiver_cmd_str(self):
+        return " ".join(self.receiver_cmd())
+
+    def sender_cmd(self, src_file):
+        return [self.cmd] + self.sender_options(src_file)
+
+    def sender_cmd_str(self, src_file):
+        return " ".join(self.sender_cmd(src_file))
+
+    @property
+    def version_option(self):
+        return "--version"
+
+    @abstractmethod
+    def match_flavor_output(self, output):
+        raise NotImplementedError
+
+
+class PrefixedTarAdapter(AbstractTarAdapter):
+    def __init__(self, prefix=""):
+        self._prefix = prefix
+        super().__init__()
+
+    @property
+    def cmd(self):
+        return f"{self._prefix}tar"
+
+
+class GnuTarAdapter(PrefixedTarAdapter):
+    def __str__(self):
+        return f"Gnu Tar [{self.cmd}]"
+
+    def receiver_options(self):
+        return ["--no-same-owner", "--extract", "--verbose"]
+
+    def sender_options(self, src_file):
+        return ["--create", src_file, "--to-stdout", "--ignore-failed-read"]
+
+    def match_flavor_output(self, output):
+        return "GNU tar" in output
+
+
+class BsdTarAdapter(AbstractTarAdapter):
+    def __str__(self):
+        return "BSD Tar"
+
+    def receiver_options(self):
+        return ["-o", "-x", "-v"]
+
+    def sender_options(self, src_file):
+        return ["-c", "-f", "-", src_file]
+
+    def match_flavor_output(self, output):
+        return "bsdtar" in output
+
+
+class BusyBoxTarAdapter(AbstractTarAdapter):
+    def __str__(self):
+        return "BusyBox Tar"
+
+    def receiver_options(self):
+        return ["x", "-v"]
+
+    def sender_options(self, src_file):
+        return ["c", "-f", "-", src_file]
+
+    def match_flavor_output(self, output):
+        return "busybox" in output
+
+
+def detect_local_tar(acceptable=None):
+    """Determine, if any, a suitable sender tar"""
+    if acceptable is None:
+        acceptable = [
+            GnuTarAdapter(),
+            GnuTarAdapter(prefix="g"),
+            BsdTarAdapter(),
+            BusyBoxTarAdapter(),
+        ]
+    for tar_flavor in acceptable:
+        try:
+            result = subprocess.run(
+                [tar_flavor.cmd, tar_flavor.version_option],
+                stdout=subprocess.PIPE,
+                check=True,
+                universal_newlines=True,
+            )
+        except (FileNotFoundError, subprocess.CalledProcessError) as e:
+            continue
+        if tar_flavor.match_flavor_output(result.stdout):
+            return tar_flavor
+    return None
+
+
+def detect_remote_tar(connection_command, acceptable=None):
+    """Determine, if any, a suitable receiver tar"""
+    if acceptable is None:
+        acceptable = [GnuTarAdapter(), GnuTarAdapter(prefix="g")]
+    for tar_flavor in acceptable:
+        try:
+            result = subprocess.run(
+                connection_command,
+                input=f"{tar_flavor.cmd} {tar_flavor.version_option}",
+                stdout=subprocess.PIPE,
+                universal_newlines=True,
+            )
+        except (FileNotFoundError, subprocess.CalledProcessError) as e:
+            raise RuntimeError(f"Error using connection command: {e}")
+        if tar_flavor.match_flavor_output(result.stdout):
+            return tar_flavor
+    return None
---
+++ tests/test_tar_adapter.py
@@ -0,0 +1,158 @@
+from abc import ABCMeta, abstractmethod
+import os
+from pathlib import Path
+
+import pytest
+
+from file_replicator.tar_adapter import (
+    GnuTarAdapter,
+    BsdTarAdapter,
+    BusyBoxTarAdapter,
+    detect_local_tar,
+    detect_remote_tar,
+)
+
+
+@pytest.mark.parametrize("prefix", ["", "g"])
+def test_gnu_tar_adapter(prefix):
+    tar = GnuTarAdapter(prefix=prefix)
+    assert tar.cmd == f"{prefix}tar"
+    assert tar.receiver_cmd() == [
+        f"{prefix}tar",
+        "--no-same-owner",
+        "--extract",
+        "--verbose",
+    ]
+    assert tar.receiver_cmd_str() == " ".join(tar.receiver_cmd())
+    assert tar.sender_cmd("foo") == [
+        f"{prefix}tar",
+        "--create",
+        "foo",
+        "--to-stdout",
+        "--ignore-failed-read",
+    ]
+    assert tar.sender_cmd_str("foo") == " ".join(tar.sender_cmd("foo"))
+
+
+def test_bsd_tar_adapter():
+    tar = BsdTarAdapter()
+    assert tar.cmd == "tar"
+    assert tar.receiver_cmd() == ["tar", "-o", "-x", "-v"]
+    assert tar.sender_cmd("foo") == [f"tar", "-c", "-f", "-", "foo"]
+
+
+# not so useful, but here we go
+def test_detect_real_local_tar():
+    tar = detect_local_tar()
+    assert isinstance(tar, (GnuTarAdapter, BsdTarAdapter, BusyBoxTarAdapter, None))
+
+
+class AbstractMockTar(metaclass=ABCMeta):
+    @property
+    @abstractmethod
+    def version():
+        raise NotImplementedError
+
+    def create_mock_tar(self):
+        return f"""#!/bin/sh
+/bin/cat << EOF
+{self.version}
+EOF
+"""
+
+
+class MockGnuTar(AbstractMockTar):
+    @property
+    def version(self):
+        return """
+tar (GNU tar) 1.31
+Copyright (C) 2019 Free Software Foundation, Inc.
+License GPLv3+: GNU GPL version 3 or later <https://gnu.org/licenses/gpl.html>.
+This is free software: you are free to change and redistribute it.
+There is NO WARRANTY, to the extent permitted by law.
+
+Written by John Gilmore and Jay Fenlason.
+"""
+
+
+class MockBsdTar(AbstractMockTar):
+    @property
+    def version(self):
+        return "bsdtar 2.8.3 - libarchive 2.8.3"
+
+
+class MockBusyBoxTar(AbstractMockTar):
+    @property
+    def version(self):
+        return "tar (busybox) 1.28.4"
+
+
+class MockUnknownTar(AbstractMockTar):
+    @property
+    def version(self):
+        return "Some Unknown tar v.1.0"
+
+
+@pytest.fixture
+def temp_dir_as_path(tmp_path, monkeypatch):
+    with monkeypatch.context() as m:
+        m.setenv("PATH", str(tmp_path))
+        yield tmp_path
+
+
+@pytest.fixture
+def mock_tar(request, temp_dir_as_path):
+    mock_tar, tar_name = request.param
+    tar_cmd = temp_dir_as_path / tar_name
+    tar_cmd.write_text(mock_tar.create_mock_tar())
+    tar_cmd.chmod(0o755)
+
+
+def which(command):
+    try:
+        path = next(
+            p
+            for p in map(Path, os.environ["PATH"].split(":"))
+            if p.is_dir() and any(f.name == "bash" for f in p.iterdir())
+        )
+        return path / command
+    except StopIteration:
+        return None
+
+
+@pytest.fixture(scope="module")
+def bash():
+    return str(which("bash"))
+
+
+ALL_TARS = [
+    GnuTarAdapter(),
+    GnuTarAdapter(prefix="g"),
+    BsdTarAdapter(),
+    BusyBoxTarAdapter(),
+]
+
+
+@pytest.mark.parametrize(
+    "mock_tar,expect",
+    [
+        ((MockGnuTar(), "tar"), GnuTarAdapter),
+        ((MockGnuTar(), "gtar"), GnuTarAdapter),
+        ((MockBsdTar(), "tar"), BsdTarAdapter),
+        ((MockBusyBoxTar(), "tar"), BusyBoxTarAdapter),
+        ((MockUnknownTar(), "tar"), type(None)),
+    ],
+    indirect=["mock_tar"],
+)
+def test_detect_tar(mock_tar, expect, bash):
+    tar = detect_local_tar(acceptable=ALL_TARS)
+    assert isinstance(tar, expect)
+    remote_tar = detect_remote_tar(bash, acceptable=ALL_TARS)
+    assert isinstance(remote_tar, expect)
+
+
+def test_no_tar_cmd(temp_dir_as_path, bash):
+    tar = detect_local_tar(acceptable=ALL_TARS)
+    assert tar is None
+    remote_tar = detect_remote_tar(bash, acceptable=ALL_TARS)
+    assert remote_tar is None
